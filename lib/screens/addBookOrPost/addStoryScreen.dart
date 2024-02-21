03ead409ae8b449e29e7415e97b8df5c12fc8599
Vyshnav Sreeshan <vyshnav.sreeshan05@gmail.com>
2024-02-22 00:46:44 +0530

    audi upload with story

--- lib/screens/addBookOrPost/addStoryScreen.dart
+++ lib/screens/addBookOrPost/addStoryScreen.dart
@@ -1,23 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:storyai/theme/pallete.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class Story {
   final String story;
   final List<String> genres;
   final String name;
+  final String? audioUrl; // Add audioUrl field
 
   Story({
     required this.story,
     required this.genres,
     required this.name,
+    this.audioUrl, // Initialize audioUrl with null
   });
 
   Map<String, dynamic> toMap() {
     return {
       'story': story,
       'genres': genres,
+      'audioUrl': audioUrl, // Include audioUrl in the map
     };
   }
 }
@@ -34,8 +41,10 @@ class _AddStoryScreenState extends State<AddStoryScreen> {
   TextEditingController _typeEditingController = TextEditingController();
   bool _storyEntered = false; // Track whether story text has been entered
   bool _typeEntered = false;
+  bool _audioPicked = false;
   List<String> _selectedGenres = []; // List to store selected genres
   User? user = FirebaseAuth.instance.currentUser;
+  File? _audioFile; // Variable to store the selected audio file
 
   // Predefined list of available genres
   final List<String> availableGenres = [
@@ -59,16 +68,25 @@ class _AddStoryScreenState extends State<AddStoryScreen> {
           story: _textEditingController.text,
           genres: _selectedGenres,
           name: name,
+          audioUrl: '', // Initialize audioUrl with an empty string
         );
 
         try {
+          // Upload audio file if available
+          String? audioUrl;
+          if (_audioFile != null) {
+            audioUrl = await uploadAudioToStorage(_audioFile!);
+          }
+
           // Add story to Firestore
           await FirebaseFirestore.instance.collection('stories').add({
             ...story.toMap(),
             'userId': uid,
             'username': name,
             'profileUrl': profileUrl,
+            'audioUrl': audioUrl, // Include audioUrl in Firestore document
           });
+
           // Clear text fields and selected genres
           _textEditingController.clear();
           _selectedGenres.clear();
@@ -81,6 +99,42 @@ class _AddStoryScreenState extends State<AddStoryScreen> {
           // Show error message
         }
       }
+    }
+  }
+
+  Future<String> uploadAudioToStorage(File audioFile) async {
+    try {
+      String fileName = DateTime.now().millisecondsSinceEpoch.toString() +
+          '.${audioFile.path.split('.').last}';
+      final Reference storageReference =
+          FirebaseStorage.instance.ref().child('audio').child(fileName);
+
+      // Upload the audio file to Firebase Storage
+      await storageReference.putFile(audioFile);
+
+      // Get the download URL of the uploaded file
+      String audioUrl = await storageReference.getDownloadURL();
+
+      print('Audio file uploaded to Firebase Storage');
+      return audioUrl;
+    } catch (e) {
+      print('Error uploading audio file: $e');
+      // Handle error if upload fails
+      throw e;
+    }
+  }
+
+  void _pickAudio() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+      allowMultiple: false,
+    );
+
+    if (result != null) {
+      setState(() {
+        _audioFile = File(result.files.single.path!);
+        _audioPicked = true;
+      });
     }
   }
 
@@ -220,7 +274,14 @@ class _AddStoryScreenState extends State<AddStoryScreen> {
                     ],
                   )
                 : SizedBox(),
+            // Add button to pick audio file
             if (_typeEntered)
+              ElevatedButton(
+                onPressed: _pickAudio,
+                child: Text('Pick Audio'),
+              ),
+            SizedBox(height: 20),
+            if (_audioPicked)
               ElevatedButton(
                 onPressed: _addStoryToFirestore,
                 child: Text('Save Story'),
