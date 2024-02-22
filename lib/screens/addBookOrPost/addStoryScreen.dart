aaf42dcfa0ece75166804e346e7172ddb7ed7632
Vyshnav Sreeshan <vyshnav.sreeshan05@gmail.com>
2024-02-22 15:32:36 +0530

    uploading progress showing

--- lib/screens/addBookOrPost/addStoryScreen.dart
+++ lib/screens/addBookOrPost/addStoryScreen.dart
@@ -1,32 +1,31 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:storyai/theme/pallete.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:storyai/theme/pallete.dart';
 
 class Story {
   final String story;
   final List<String> genres;
   final String name;
   final String? audioUrl;
-  final DateTime createdAt; // Add audioUrl field
+  final DateTime createdAt;
 
   Story({
     required this.story,
     required this.genres,
     required this.name,
     this.audioUrl,
-    required this.createdAt, // Initialize audioUrl with null
+    required this.createdAt,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'story': story,
       'genres': genres,
-      'audioUrl': audioUrl, // Include audioUrl in the map
+      'audioUrl': audioUrl,
     };
   }
 }
@@ -41,14 +40,15 @@ class AddStoryScreen extends StatefulWidget {
 class _AddStoryScreenState extends State<AddStoryScreen> {
   TextEditingController _textEditingController = TextEditingController();
   TextEditingController _typeEditingController = TextEditingController();
-  bool _storyEntered = false; // Track whether story text has been entered
+  bool _storyEntered = false;
   bool _typeEntered = false;
   bool _audioPicked = false;
-  List<String> _selectedGenres = []; // List to store selected genres
+  List<String> _selectedGenres = [];
   User? user = FirebaseAuth.instance.currentUser;
-  File? _audioFile; // Variable to store the selected audio file
+  File? _audioFile;
+  bool _isUploading = false;
+  bool _isUploaded = false;
 
-  // Predefined list of available genres
   final List<String> availableGenres = [
     'Adventure',
     'Mystery',
@@ -58,51 +58,55 @@ class _AddStoryScreenState extends State<AddStoryScreen> {
   ];
 
   void _addStoryToFirestore() async {
-    if (_storyEntered) {
-      // Get user information
+    if (_storyEntered && _typeEntered) {
+      setState(() {
+        _isUploading = true;
+      });
+
       if (user != null) {
         final String uid = user!.uid;
         final String name = user!.displayName ?? '';
         final String profileUrl = user!.photoURL ?? '';
 
-        // Create the story object
         final story = Story(
           story: _textEditingController.text,
           genres: _selectedGenres,
           name: name,
           audioUrl: '',
-          createdAt: DateTime.now()
-              .toUtc(), // Initialize audioUrl with an empty string
+          createdAt: DateTime.now(),
         );
 
         try {
-          // Upload audio file if available
           String? audioUrl;
           if (_audioFile != null) {
             audioUrl = await uploadAudioToStorage(_audioFile!);
           }
 
-          // Add story to Firestore
           await FirebaseFirestore.instance.collection('stories').add({
             ...story.toMap(),
             'userId': uid,
             'username': name,
             'profileUrl': profileUrl,
             'audioUrl': audioUrl,
-            'createdAt': DateTime.now()
-                .toUtc(), // Include audioUrl in Firestore document
+            'createdAt': DateTime.now().toUtc(),
           });
 
-          // Clear text fields and selected genres
+          setState(() {
+            _isUploaded = true;
+            _isUploading = false;
+          });
+
           _textEditingController.clear();
           _selectedGenres.clear();
           _storyEntered = false;
-          setState(() {});
+
+          // Navigate to the previous screen after upload
           Navigator.pop(context);
-          // Show success message or navigate to another screen
         } catch (e) {
           print('Error adding story to Firestore: $e');
-          // Show error message
+          setState(() {
+            _isUploading = false;
+          });
         }
       }
     }
@@ -115,17 +119,14 @@ class _AddStoryScreenState extends State<AddStoryScreen> {
       final Reference storageReference =
           FirebaseStorage.instance.ref().child('audio').child(fileName);
 
-      // Upload the audio file to Firebase Storage
       await storageReference.putFile(audioFile);
 
-      // Get the download URL of the uploaded file
       String audioUrl = await storageReference.getDownloadURL();
 
       print('Audio file uploaded to Firebase Storage');
       return audioUrl;
     } catch (e) {
       print('Error uploading audio file: $e');
-      // Handle error if upload fails
       throw e;
     }
   }
@@ -144,163 +145,194 @@ class _AddStoryScreenState extends State<AddStoryScreen> {
     }
   }
 
+  Widget _uploadButton() {
+    if (_isUploading) {
+      return ElevatedButton(
+        onPressed: null,
+        child: Text('Uploading...'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.grey,
+        ),
+      );
+    } else if (_isUploaded) {
+      return ElevatedButton(
+        onPressed: () => Navigator.pop(context),
+        child: Text('Uploaded'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.green,
+        ),
+      );
+    } else {
+      return ElevatedButton(
+        onPressed: _addStoryToFirestore,
+        child: Text('Save Story'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Pallete.brown, // Change to your desired color
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          elevation: 0,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios, color: Pallete.brown),
-            onPressed: () => Navigator.pop(context, false),
-          ),
-          title: Text(
-            "Add Story",
-            style: TextStyle(
-              color: Pallete.brown,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () => Navigator.pop(context, false),
+        ),
+        title: Text(
+          "Add Story",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: _textEditingController,
-                  decoration: InputDecoration(
-                    hintText: 'Write your short story here',
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _textEditingController,
+                decoration: InputDecoration(
+                  hintText: 'Write your short story here',
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
                   ),
-                  maxLines: null,
-                  maxLength: 1000, // Allow multiple lines of text
-                  onChanged: (text) {
-                    setState(() {
-                      _storyEntered =
-                          text.isNotEmpty; // Track if story is entered
-                    });
-                  },
                 ),
-                SizedBox(height: 20),
-                _storyEntered
-                    ? Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Select Genre(s):',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
+                maxLines: null,
+                maxLength: 1000,
+                onChanged: (text) {
+                  setState(() {
+                    _storyEntered = text.isNotEmpty;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              _storyEntered
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Select Genre(s):',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
                           ),
-                          SizedBox(height: 10),
-                          Autocomplete<String>(
-                            optionsBuilder:
-                                (TextEditingValue textEditingValue) {
-                              if (textEditingValue.text.isEmpty) {
-                                return const Iterable<String>.empty();
-                              }
-                              return availableGenres.where((String option) {
-                                return option.toLowerCase().contains(
-                                      textEditingValue.text.toLowerCase(),
-                                    );
-                              });
-                            },
-                            onSelected: (String selection) {
-                              setState(() {
-                                _selectedGenres.add(selection);
-                                _typeEditingController.text = selection;
-                                _typeEditingController.clear();
-                                _typeEntered = _selectedGenres.isNotEmpty;
-                              });
-                            },
-                            fieldViewBuilder: (BuildContext context,
-                                TextEditingController
-                                    fieldTextEditingController,
-                                FocusNode fieldFocusNode,
-                                VoidCallback onFieldSubmitted) {
-                              _typeEditingController =
-                                  fieldTextEditingController;
-                              return TextField(
-                                controller: fieldTextEditingController,
-                                focusNode: fieldFocusNode,
-                                decoration: InputDecoration(
-                                  hintText: 'Type of the story',
-                                ),
-                              );
-                            },
-                            optionsViewBuilder: (BuildContext context,
-                                AutocompleteOnSelected<String> onSelected,
-                                Iterable<String> options) {
-                              return Align(
-                                alignment: Alignment.topLeft,
-                                child: Material(
-                                  elevation: 4.0,
-                                  child: SizedBox(
-                                    height: 200.0,
-                                    child: ListView.builder(
-                                      padding: EdgeInsets.zero,
-                                      itemCount: options.length,
-                                      itemBuilder:
-                                          (BuildContext context, int index) {
-                                        final String option =
-                                            options.elementAt(index);
-                                        return GestureDetector(
-                                          onTap: () {
-                                            onSelected(option);
-                                          },
-                                          child: ListTile(
-                                            title: Text(option),
-                                          ),
-                                        );
-                                      },
-                                    ),
+                        ),
+                        SizedBox(height: 10),
+                        Autocomplete<String>(
+                          optionsBuilder: (TextEditingValue textEditingValue) {
+                            if (textEditingValue.text.isEmpty) {
+                              return const Iterable<String>.empty();
+                            }
+                            return availableGenres.where((String option) {
+                              return option.toLowerCase().contains(
+                                    textEditingValue.text.toLowerCase(),
+                                  );
+                            });
+                          },
+                          onSelected: (String selection) {
+                            setState(() {
+                              _selectedGenres.add(selection);
+                              _typeEditingController.text = selection;
+                              _typeEditingController.clear();
+                              _typeEntered = _selectedGenres.isNotEmpty;
+                            });
+                          },
+                          fieldViewBuilder: (BuildContext context,
+                              TextEditingController fieldTextEditingController,
+                              FocusNode fieldFocusNode,
+                              VoidCallback onFieldSubmitted) {
+                            _typeEditingController = fieldTextEditingController;
+                            return TextField(
+                              controller: fieldTextEditingController,
+                              focusNode: fieldFocusNode,
+                              decoration: InputDecoration(
+                                hintText: 'Type of the story',
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
                                   ),
                                 ),
-                              );
-                            },
-                          ),
-                          SizedBox(height: 20),
-                          // Display selected genres
-                          Wrap(
-                            children: _selectedGenres
-                                .map(
-                                  (genre) => Chip(
-                                    label: Text(genre),
-                                    onDeleted: () {
-                                      setState(() {
-                                        _selectedGenres.remove(genre);
-                                        _typeEntered =
-                                            _selectedGenres.isNotEmpty;
-                                      });
+                              ),
+                            );
+                          },
+                          optionsViewBuilder: (BuildContext context,
+                              AutocompleteOnSelected<String> onSelected,
+                              Iterable<String> options) {
+                            return Align(
+                              alignment: Alignment.topLeft,
+                              child: Material(
+                                elevation: 4.0,
+                                child: SizedBox(
+                                  height: 200.0,
+                                  child: ListView.builder(
+                                    padding: EdgeInsets.zero,
+                                    itemCount: options.length,
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      final String option =
+                                          options.elementAt(index);
+                                      return GestureDetector(
+                                        onTap: () {
+                                          onSelected(option);
+                                        },
+                                        child: ListTile(
+                                          title: Text(option),
+                                        ),
+                                      );
                                     },
                                   ),
-                                )
-                                .toList(),
-                          ),
-                          SizedBox(height: 20),
-                        ],
-                      )
-                    : SizedBox(),
-                // Add button to pick audio file
-                if (_typeEntered)
-                  ElevatedButton(
-                    onPressed: _pickAudio,
-                    child: Text('Pick Audio'),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Wrap(
+                          children: _selectedGenres
+                              .map(
+                                (genre) => Chip(
+                                  label: Text(genre),
+                                  onDeleted: () {
+                                    setState(() {
+                                      _selectedGenres.remove(genre);
+                                      _typeEntered = _selectedGenres.isNotEmpty;
+                                    });
+                                  },
+                                ),
+                              )
+                              .toList(),
+                        ),
+                        SizedBox(height: 20),
+                      ],
+                    )
+                  : SizedBox(),
+              if (_typeEntered)
+                ElevatedButton(
+                  onPressed: _pickAudio,
+                  child: Text('Pick Audio'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Pallete.brown,
                   ),
-                SizedBox(height: 20),
-                if (_audioPicked)
-                  ElevatedButton(
-                    onPressed: _addStoryToFirestore,
-                    child: Text('Save Story'),
-                  )
-              ],
-            ),
+                ),
+              SizedBox(height: 20),
+              if (_audioPicked) _uploadButton(),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
